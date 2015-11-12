15cb0e8ac4b92e6337db1b62256bb221b025f8c7
Andrey Kutejko <aku@anahoret.com>
2015-11-12 17:33:00 +0200

    initial

---
+++ webpack.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: __dirname + '/public/assets/',
+    publicPath: '/assets/',
+    filename: 'index.js'
+  },
+  module: {
+    loaders: [
+      { test: /\.js$/,  loader: "babel-loader", exclude: /node_modules/, query: { presets: ['es2015'], plugins: ['transform-runtime'] } }
+    ],
+    preLoaders: [
+      { test: /\.js$/, loader: "eslint-loader", exclude: /node_modules|tests/ }
+    ]
+  },
+  eslint: {
+    configPath: __dirname + ".eslintrc"
+  }
+};
+
