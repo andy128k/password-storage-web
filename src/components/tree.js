45d13bd355dde84581e19f4d4a0286582d04329d
Andrey Kutejko <andy128k@gmail.com>
2015-11-20 19:06:42 +0200

    refactor tree component

--- src/components/tree.js
+++ src/components/tree.js
@@ -1,28 +1,26 @@
 import React from 'react';
 
-class TreeNode extends React.Component {
-  render() {
-    return React.DOM.div({className: 'tree__node tree__node_level' + this.props.level},
-      this.renderRow(),
-      this.props.entry.children.map((entry, key) =>
-        React.createElement(TreeNode, {key, entry, level: this.props.level + 1, onClick: this.props.onClick}))
-    );
-  }
-
-  renderRow() {
-    return React.DOM.div({className: 'tree__row', onClick: this.props.onClick.bind(null, this.props.entry)},
-      this.props.entry.name
-    );
-  }
-}
-
-TreeNode.displayName = 'TreeNode';
-
 class Tree extends React.Component {
   render() {
     return React.DOM.div({className: 'tree'},
-      this.props.entries.map((entry, key) =>
-        React.createElement(TreeNode, {key, entry, level: 0, onClick: this.props.onClick}))
+      this.renderEntries(this.props.entries, 0)
+    );
+  }
+
+  renderEntries(entries, level) {
+    return entries.map((entry, key) => this.renderNode(entry, level, key));
+  }
+
+  renderNode(entry, level, key) {
+    return React.DOM.div({className: 'tree__node tree__node_level' + level, key},
+      this.renderRow(entry),
+      this.renderEntries(entry.children, level + 1)
+    );
+  }
+
+  renderRow(entry) {
+    return React.DOM.div({className: 'tree__row', onClick: this.props.onClick.bind(null, entry)},
+      entry.name
     );
   }
 }
