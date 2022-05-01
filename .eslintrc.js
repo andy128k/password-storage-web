95343401fcce29ff643d77530cb6fb1303335e77
Andrey Kutejko <andy128k@gmail.com>
2022-05-01 11:55:35 +0200

    change ESLint config format from YAML to JS

---
+++ .eslintrc.js
@@ -0,0 +1,37 @@
+module.exports = {
+  env: {
+    browser: true,
+    node: true,
+    es6: true,
+  },
+  parser: "@babel/eslint-parser",
+  parserOptions: {
+    sourceType: "module",
+    jsx: true,
+  },
+  plugins: ["react", "jest"],
+  extends: [
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:react-hooks/recommended",
+    "plugin:jest/recommended",
+  ],
+  rules: {
+    quotes: ["off", "double"],
+    camelcase: "off",
+    semi: "warn",
+    "no-use-before-define": ["error", "nofunc"],
+    eqeqeq: "warn",
+    "no-alert": "warn",
+    "no-unused-vars": "error",
+    "react/prop-types": "off",
+    "react/jsx-no-undef": "error",
+    "react/jsx-no-bind": "error",
+    "react/no-unknown-property": "error",
+  },
+  settings: {
+    react: {
+      version: "16.4",
+    },
+  },
+};
